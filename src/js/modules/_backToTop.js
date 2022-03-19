848bac1ae40c0e21f0d0b3b3d13e94221d481f18
commudeMart <79067485+commudeMart@users.noreply.github.com>
2022-03-19 10:30:24 +0800

    remove temp and add static

--- src/js/modules/_backToTop.js
+++
@@ -1,6 +0,0 @@
-export const backToTop = () => {
-  $('.js-toTop').click(function (e) {
-    e.preventDefault();
-    $('html, body').delay(300).animate({ scrollTop: 0 }, 800);
-  });
-};
--- src/js/modules/_stickyHeader.js
+++
@@ -1,4 +0,0 @@
-export const stickyHeader = () => {
-  $(window).scrollTop() > 0 ? $('header').addClass('is-sticky') : $('header').removeClass('is-sticky');
-  // add the sticky settings thru css
-};
